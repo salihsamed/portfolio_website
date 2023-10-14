48cf8dc1dda19429cd786fdfe0975686e305fe68
salihsamed <salihsamedavsar@gmail.com>
2023-10-14 15:14:14 +0300

    changes

--- src/components/AbilitySection.jsx
+++ src/components/AbilitySection.jsx
@@ -9,8 +9,11 @@ import mongodbIcon from '../assets/mongodb.png'
 import tailwindIcon from '../assets/tailwind.png'
 import Ability from '../components/Ability';
 import expressIcon from '../assets/express.png'
+import aspIcon from '../assets/asp_mvc.png'
+import cIcon from '../assets/c_sharp.png'
+import jqueryIcon from '../assets/jquery.png'
+import sqlIcon from '../assets/ms_sql.jpg'
 import { useTranslation } from 'react-i18next'
-
 
 
 const AbilitySection = () => {
@@ -52,11 +55,27 @@ const AbilitySection = () => {
         {
             image:expressIcon,
             text:"Express.js"
+        },
+        {
+            image:aspIcon,
+            text:"ASP.NET MVC"
+        },
+        {
+            image:cIcon,
+            text:"C#"
+        },
+        {
+            image:jqueryIcon,
+            text:"jQuery"
+        },
+        {
+            image:sqlIcon,
+            text:"Microsoft SQL"
         }
     ]
 
   return (
-    <div id='ability' className='h-screen w-full px-20 max-lg:px-2'>
+    <div id='ability' className='h-auto w-full px-20 lg:pb-20 max-lg:pb-10 max-lg:px-2'>
         <div className='text-[4rem] max-sm:text-[3rem] max-lg:text-center'>{t("Yetenekler")}</div>
         <div className='grid grid-cols-5 max-lg:grid-cols-3 max-2xl:grid-cols-4 mt-2 gap-10 max-sm:gap-2 max-sm:mt-10'>
             {abilities.map((a,index)=>(<Ability key={index} image={a.image} text={a.text}/>))}
